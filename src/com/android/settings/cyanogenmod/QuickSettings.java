aa9e6cbefb4b9d88bb92c01e9c2a811ff9619ff9
Schubi <schubi@erlangen.ccc.de>
2013-08-15 00:09:59 +0200

    Quick Settings: Option to use floating window (1/2)
    Allows the option of launching preferences from the quicksettings menu in a
    floating window
    
    Change-Id: I267cae3cddaf8be697e5c60e21f7ba2616344a19
    Signed-off-by: Evan Anderson <evan1124@gmail.com>
    
    Conflicts:
    	res/values/pa_strings.xml
    	src/com/android/settings/cyanogenmod/QuickSettings.java

--- src/com/android/settings/cyanogenmod/QuickSettings.java
+++ src/com/android/settings/cyanogenmod/QuickSettings.java
@@ -61,6 +61,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
     private static final String STATIC_TILES = "static_tiles";
     private static final String DYNAMIC_TILES = "pref_dynamic_tiles";
     private static final String PREF_FLIP_QS_TILES = "flip_qs_tiles";
+    private static final String FLOATING_WINDOW ="floating_window";
 
     MultiSelectListPreference mRingMode;
     ListPreference mNetworkMode;
@@ -73,6 +74,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
     CheckBoxPreference mDynamicUsbTether;
     CheckBoxPreference mCollapsePanel;
     CheckBoxPreference mFlipQsTiles;
+    CheckBoxPreference mFloatingWindow;
     ListPreference mQuickPulldown;
     PreferenceCategory mGeneralSettings;
     PreferenceCategory mStaticTiles;
@@ -106,6 +108,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
 
         mCollapsePanel = (CheckBoxPreference) prefSet.findPreference(COLLAPSE_PANEL);
         mCollapsePanel.setChecked(Settings.System.getInt(resolver, Settings.System.QS_COLLAPSE_PANEL, 0) == 1);
+        
+        mFloatingWindow = (CheckBoxPreference) prefSet.findPreference(FLOATING_WINDOW);
+        mFloatingWindow.setChecked(Settings.System.getInt(resolver, Settings.System.QS_FLOATING_WINDOW, 0) == 1);
 
         // Add the sound mode
         mRingMode = (MultiSelectListPreference) prefSet.findPreference(EXP_RING_MODE);
@@ -226,6 +231,10 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
                     Settings.System.QUICK_SETTINGS_TILES_FLIP,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
             return true; 
+        } else if (preference == mFloatingWindow) {
+            Settings.System.putInt(resolver, Settings.System.QS_FLOATING_WINDOW,
+                    mFloatingWindow.isChecked() ? 1 : 0);
+            return true;            
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
